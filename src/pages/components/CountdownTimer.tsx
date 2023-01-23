95f14b4584da2e4fe60dbd18a5943be724528b63
Emmanuel Sylvester <emmanuelsylvester@emansyl.myfiosgateway.com>
2023-01-23 00:27:40 -0500

    Add multiple counters

--- src/pages/components/CountdownTimer.tsx
+++ src/pages/components/CountdownTimer.tsx
@@ -1,59 +1,59 @@
-import React from 'react';
-import { useCountdown } from '../../hooks/useCountdown';
-import  DateTimeDisplay  from './DateTimeDisplay'
+import React from "react";
+import { useCountdown } from "../../hooks/useCountdown";
+import DateTimeDisplay from "./DateTimeDisplay";
 
 interface Count {
-    days: number,
-    hours: number,
-    minutes: number,
-    seconds: number
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
 }
 
 const ExpiredNotice = () => {
-    return (
-      <div className="expired-notice">
-        <span>Expired!!!</span>
-        <p>Please select a future date and time.</p>
+  return (
+    <div className="expired-notice">
+      <span>Expired!!!</span>
+      <p>Please select a future date and time.</p>
+    </div>
+  );
+};
+
+const ShowCounter = ({ days, hours, minutes, seconds }: Count) => {
+  return (
+    <div className="show-counter">
+      <div className="countdown-link">
+        <DateTimeDisplay value={days} type={"Days"} isDanger={days <= 3} />
+        <p>:</p>
+        <DateTimeDisplay value={hours} type={"Hours"} isDanger={false} />
+        <p>:</p>
+        <DateTimeDisplay value={minutes} type={"Mins"} isDanger={false} />
+        <p>:</p>
+        <DateTimeDisplay value={seconds} type={"Seconds"} isDanger={false} />
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-
-  const ShowCounter = ({ days, hours, minutes, seconds } : Count) => {
-    return (
-      <div className="show-counter">
-           <div className="countdown-link">
-       
-        <DateTimeDisplay value={days} type={'Days'} isDanger={days <= 3} />
-        <p>:</p>
-        <DateTimeDisplay value={hours} type={'Hours'} isDanger={false} />
-        <p>:</p>
-        <DateTimeDisplay value={minutes} type={'Mins'} isDanger={false} />
-        <p>:</p>
-        <DateTimeDisplay value={seconds} type={'Seconds'} isDanger={false} />
-        </div>
-      </div>
-    );
-  };
-  
-  
-
-const CountdownTimer = ({ targetDate } : {targetDate : Date}) => {
+const CountdownTimer = ({ targetDate, nameOfCarnival, color }: { targetDate: Date, nameOfCarnival: string,color: string}) => {
   const [days, hours, minutes, seconds] = useCountdown(targetDate);
 
   if (days + hours + minutes + seconds <= 0) {
     return <ExpiredNotice />;
   } else {
     return (
-      <ShowCounter
-        days={days}
-        hours={hours}
-        minutes={minutes}
-        seconds={seconds}
-      />
+      <div className={`timerBox ${color}`}>
+        <h1>Countdown</h1>
+        <h1>To</h1>
+        <h1>{nameOfCarnival}</h1>
+        <ShowCounter
+          days={days}
+          hours={hours}
+          minutes={minutes}
+          seconds={seconds}
+        />
+      </div>
     );
   }
 };
-
 
 export default CountdownTimer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,15 @@ import CountDownTimer from './components/CountdownTimer'
 export default function Home() {
 
   let TRINIDAD_CARNIVAL_DATE_IN_MS = new Date(2023,1,20);
+  let JAMAICA_CARNIVAL_DATE_IN_MS = new Date(2023,3,16);
+  let CROPOVER_CARNIVAL_DATE_IN_MS = new Date(2023,7,7);
 
   return (
-    <div className='timerBox'>
-      <h1>Countdown</h1>
-      <h1>To</h1>
-      <h1>Trinidad Carnival</h1>
-      <CountDownTimer targetDate = {TRINIDAD_CARNIVAL_DATE_IN_MS}/>
+    <div >
+      
+      <CountDownTimer targetDate = {TRINIDAD_CARNIVAL_DATE_IN_MS} nameOfCarnival={'Trinidad Carnival'} color={'purple'}/>
+      <CountDownTimer targetDate = {JAMAICA_CARNIVAL_DATE_IN_MS} nameOfCarnival={'Jamaica Carnival'} color={'blue'}/>
+      <CountDownTimer targetDate = {CROPOVER_CARNIVAL_DATE_IN_MS} nameOfCarnival={'Cropover'} color={'aquamarine'}/>
     </div>
   )
 }
